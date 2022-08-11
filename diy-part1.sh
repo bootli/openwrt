fa36107c7f6bf115f1a4b60bf41cc64533919521
OpenWrtLi <107589430+OpenWrtLi@users.noreply.github.com>
2022-08-11 17:34:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,62 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ##############################################################################################
+#删除插件
+rm -rf package/li
+rm -rf feeds/kenzo/luci-app-adguardhome
+rm -rf feeds/kenzo/adguardhome
+rm -rf feeds/kenzo/luci-app-godproxy
+rm -rf feeds/kenzo/luci-app-ikoolproxy
+rm -rf feeds/kenzo/luci-app-tencentddns
+rm -rf feeds/kenzo/diy/.packages/luci-app-adguardhome
+rm -rf feeds/kenzo/diy/.packages/adguardhome
+rm -rf feeds/kenzo/luci-theme-neobird
+rm -rf feeds/kenzo/luci-theme-edge
+rm -rf feeds/luci/applications/luci-app-v2ray-server
+rm -rf feeds/luci/applications/luci-app-socat
+rm -rf feeds/kenzo/luci-app-upnp
+rm -rf feeds/luci/applications/luci-app-aliyundrive-fuse
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-amule
+rm -rf feeds/luci/applications/luci-app-diskman
+rm -rf feeds/kenzo/luci-app-openvpn-server
+rm -rf feeds/kenzo/luci-app-softethervpn
+rm -rf feeds/luci/applications/luci-app-baidupcs-web
+rm -rf feeds/kenzo/luci-app-wrtbwmon
+rm -rf feeds/kenzo/luci-app-xlnetacc
+rm -rf feeds/kenzo/luci-app-tencentddns
+rm -rf feeds/kenzo/luci-app-jd-dailybonus
+rm -rf feeds/luci/applications/luci-app-v2ray-server
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/kenzo/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-edge
+rm -rf package/lean/luci-theme-neobird
+rm -rf package/lean/luci-theme-opentopd
 
 # 主题
-git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon 
+git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/lean/luci-theme-edge
+git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd
 
 ##############################################################################################
+#添加插件
+git clone https://github.com/bootli/luci-app-v2ray-server.git feeds/luci/applications/luci-app-v2ray-server
+mkdir package/li
+git clone https://github.com/bootli/luci-app-ikoolproxy.git package/li/luci-app-ikoolproxy
+git clone https://github.com/bootli/luci-app-tencentddns.git package/li/luci-app-tencentddns
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/li/luci-app-adguardhome
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/li/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-ttnode.git package/li/luci-app-ttnode
+git clone https://github.com/messense/aliyundrive-fuse.git package/li/aliyundrive-fuse
+git clone https://github.com/bootli/luci-app-beardropper.git package/li/luci-app-beardropper
+git clone https://github.com/sirpdboy/netspeedtest.git package/li/netspeedtest
+git clone https://github.com/destan19/OpenAppFilter.git package/li/OpenAppFilter
+git clone https://github.com/bootli/luci-app-amule.git package/li/luci-app-amule
+git clone https://github.com/bootli/luci-app-baidupcs-web.git package/li/luci-app-baidupcs-web
+git clone https://github.com/bootli/luci-app-socat.git package/li/luci-app-socat
+git clone https://github.com/bootli/luci-app-linkease.git  package/li/luci-app-linkease
 
 
 # 状态
